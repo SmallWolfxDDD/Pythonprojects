00e386ff0c76fe3116ac6d0a5efeb74880951f23
SmallWolfxDDD <96128344+SmallWolfxDDD@users.noreply.github.com>
2022-11-17 21:05:59 +0800

    Update Mix40-V1.py

--- Mix40-V1.py
+++ Mix40-V1.py
@@ -2,6 +2,7 @@ from itertools import permutations
 def p(list1):
     return list(permutations(list1))
 def findway(list):
+    time = 0
     list1 = p(list)
     for i in list1:
         a, b, c = 1, 1, 1
@@ -10,8 +11,9 @@ def findway(list):
             ans = do(i[0], i[1], a)
             ans = do(ans, i[2], b)
             ans = do(ans, i[3], c)
+            time += 1
             if ans == 40:
-                return str(i[0]) + type1(a) + str(i[1]) + type1(b) + str(i[2]) + type1(c) + str(i[3])
+                return str(i[0]) + type1(a) + str(i[1]) + type1(b) + str(i[2]) + type1(c) + str(i[3]), time
             else:
                 a += 1
                 if a == 5:
@@ -19,7 +21,8 @@ def findway(list):
                     b += 1
                 if b == 5:
                     b = 1
-                    c += 1
+                    c += 11
+    return "No solver!!!", time
 def do(a, b, c):
     if c == 1: return a + b
     if c == 2: return a - b
@@ -30,8 +33,11 @@ def type1(a):
     if a == 2: return "-"
     if a == 3: return "*"
     if a == 4: return "/"
-liste = []
-for e in range(1, 5):
-    e1 = input(str(e) + ":")
-    liste.append(int(e1))
-print(findway(liste))
+while True:
+    liste = []
+    for e in range(1, 5):
+        e1 = input(str(e) + ":")
+        liste.append(int(e1))
+    ans, time = findway(liste)
+    print(ans)
+    print("Find / Not find the Ans in " + str(time) + " times")
