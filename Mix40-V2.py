92db9a0755897c231784524c1dca74c76235d024
SmallWolfxDDD <96128344+SmallWolfxDDD@users.noreply.github.com>
2022-11-17 21:09:52 +0800

    Update and rename Mix40-V1.py to Mix40-V2.py

--- Mix40-V2.py
+++ Mix40-V2.py
@@ -1,27 +1,22 @@
 from itertools import permutations
-def p(list1):
-    return list(permutations(list1))
+def p(list1): return list(permutations(list1))
 def findway(list):
-    time = 0
-    list1 = p(list)
+    list1, time = p(list), 0
     for i in list1:
         a, b, c = 1, 1, 1
         while not a + b + c >= 12:
             ans = 0
             ans = do(i[0], i[1], a)
             ans = do(ans, i[2], b)
-            ans = do(ans, i[3], c)
-            time += 1
+            ans, time = do(ans, i[3], c), time + 1
             if ans == 40:
                 return str(i[0]) + type1(a) + str(i[1]) + type1(b) + str(i[2]) + type1(c) + str(i[3]), time
             else:
                 a += 1
                 if a == 5:
-                    a = 1
-                    b += 1
+                    a, b = 1, b + 1
                 if b == 5:
-                    b = 1
-                    c += 11
+                    b, c = 1, c + 1
     return "No solver!!!", time
 def do(a, b, c):
     if c == 1: return a + b
